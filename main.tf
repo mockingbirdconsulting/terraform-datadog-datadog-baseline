906510a9d3ab09b21908284687eddc7fc27e7074
Matthew Macdonald-Wallace <matt@mockingbirdconsulting.co.uk>
2018-12-06 09:54:43 +0000

    Update formatting

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 # Configure the Datadog provider
-provider "datadog" {
-}
+provider "datadog" {}
 
 # If Free RAM is below 30%, send an alert
 resource "datadog_monitor" "check_ram" {
-  name               = "Check we have enough free RAM across systems"
-  type               = "metric alert"
-  message            = "Free Memory Alert on {{host.name}}. Notify: ${var.primary_notifier}"
+  name    = "Check we have enough free RAM across systems"
+  type    = "metric alert"
+  message = "Free Memory Alert on {{host.name}}. Notify: ${var.primary_notifier}"
 
   query = "avg(last_5m):avg:system.mem.pct_usable{*} <= 0.3"
 
@@ -15,18 +14,18 @@ resource "datadog_monitor" "check_ram" {
     critical_recovery = 0.5
   }
 
-  notify_no_data    = false
-  notify_audit = true
-  include_tags = true
+  notify_no_data = false
+  notify_audit   = true
+  include_tags   = true
 
   tags = ["system:memory", "system:core_metrics"]
 }
 
 # If 5m load average goes over a particular level, send an alert
 resource "datadog_monitor" "check_5m_load_average" {
-  name               = "Check the 5 minute load average"
-  type               = "metric alert"
-  message            = "Load Average Alert on {{host.name}}. Notify: ${var.primary_notifier}"
+  name    = "Check the 5 minute load average"
+  type    = "metric alert"
+  message = "Load Average Alert on {{host.name}}. Notify: ${var.primary_notifier}"
 
   query = "avg(last_5m):avg:system.load.5{*}"
 
@@ -35,9 +34,9 @@ resource "datadog_monitor" "check_5m_load_average" {
     critical_recovery = 0.7
   }
 
-  notify_no_data    = false
-  notify_audit = true
-  include_tags = true
+  notify_no_data = false
+  notify_audit   = true
+  include_tags   = true
 
   tags = ["system:memory", "system:core_metrics"]
 }
